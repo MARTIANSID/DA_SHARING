e3d2c1107bdadc5ba6d6d6062ec60f6b456f0c67
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2020-10-18 00:05:00 +0530

    BAC

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../constants/themes.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -8,7 +9,13 @@ class Profile extends StatefulWidget {
 class _ProfileState extends State<Profile> {
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
+      height: size.height,
+      width: size.width,
+      decoration: BoxDecoration(
+        color: ThemeConstants.BGCOLOR_DARK,
+      ),
       child: Center(child: Text("Profile")),
     );
   }
