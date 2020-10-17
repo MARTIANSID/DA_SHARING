b289315c097df0ad9db2298b3bf0fc1b60e2d5d4
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2020-10-18 00:14:22 +0530

    Update home.dart

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:videoPlayer/screens/chatRoom.dart';
 import 'package:videoPlayer/screens/profile.dart';
 import 'package:videoPlayer/screens/upload.dart';
+import '../constants/themes.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -19,31 +20,35 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: widgetOptions[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Color(0xffEA4C89),
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        }, // this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
-          ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.mail),
-            title: new Text('Messages'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.file_upload),
-            title: Text('Profile'),
-          ),
-        ],
-        elevation: 4,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(gradient: ThemeConstants.PURPLE_GRADIENT),
+        child: BottomNavigationBar(
+          backgroundColor: Colors.transparent,
+          type: BottomNavigationBarType.fixed,
+          selectedItemColor: ThemeConstants.BGCOLOR_DARK_COMP,
+          currentIndex: currentIndex,
+          onTap: (index) {
+            setState(() {
+              currentIndex = index;
+            });
+          }, // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text('Home'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.mail),
+              title: new Text('Messages'),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.file_upload),
+              title: Text('Profile'),
+            ),
+          ],
+          elevation: 4,
+        ),
       ),
     );
   }
 }
-
