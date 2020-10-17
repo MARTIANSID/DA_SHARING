efb9f871ec018e11527304e87434d5bad4d404cc
rishabhmishra23599 <52506383+rishabhmishra23599@users.noreply.github.com>
2020-10-18 00:04:35 +0530

    background

--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -13,3 +13,32 @@ class _ProfileState extends State<Profile> {
     );
   }
 }
+
+class NiceBackground extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    // TODO: implement paint
+    var height = size.height;
+    var width = size.width;
+
+    var myPath = Path()
+      ..lineTo(0, height / 2)
+      ..quadraticBezierTo(
+          width / 4, height / 4 + height / 2, width / 2, height / 2)
+      ..quadraticBezierTo(width / 4 + width / 2, height / 4, width, height / 2)
+      ..lineTo(width, 0)
+      ..close();
+
+    Rect rect = Rect.largest;
+
+    var paintBrush = Paint()
+      ..color = Color(0xff226DC6)
+      ..strokeWidth = 4
+      ..style = PaintingStyle.fill;
+
+    canvas.drawPath(myPath, paintBrush);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => true;
+}
