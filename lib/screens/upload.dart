71b933d0e2c00546a17d322696d5bc8823f1c3e5
MARTIANSID <59787009+MARTIANSID@users.noreply.github.com>
2020-10-19 03:02:08 +0530

    d

--- lib/screens/upload.dart
+++ lib/screens/upload.dart
@@ -1,3 +1,7 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+// import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 class Upload extends StatefulWidget {
@@ -8,8 +12,21 @@ class Upload extends StatefulWidget {
 class _UploadState extends State<Upload> {
   @override
   Widget build(BuildContext context) {
+    Future<void> selectFile() async {
+      FilePickerResult result = await FilePicker.platform.pickFiles();
+
+      if (result != null) {
+        File file = File(result.files.single.path);
+      }
+    }
+
     return Container(
-      child: Center(child: Text("Upload")),
+      child: Center(
+          child: GestureDetector(
+              onTap: () async {
+                await selectFile();
+              },
+              child: Text("Upload"))),
     );
   }
 }
